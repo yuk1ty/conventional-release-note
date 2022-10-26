21271d33d47d86b34ce7cc1c8131476638288bb2
yuk1ty <yuk1ty@users.noreply.github.com>
2022-10-26 16:53:14 +0900

    feat: get commit log from raw git log

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import {ExecOptions} from '@actions/exec'
 import * as Option from 'fp-ts/Option'
 import * as Either from 'fp-ts/Either'
 import * as TaskEither from 'fp-ts/TaskEither'
@@ -6,20 +8,46 @@ import {pipe} from 'fp-ts/lib/function'
 import {categorize} from './classifier'
 import {generateDoc, generateReleaseNote} from './generator'
 
+const execute = (command: string) => {
+  let output = ''
+  const options: ExecOptions = {}
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      output += data.toString()
+    },
+    stderr: (data: Buffer) => {
+      console.error(data)
+    }
+  }
+  Promise.resolve(exec.exec(command, [], options))
+  return TaskEither.of(output)
+}
+
 async function run(): Promise<void> {
   const program = pipe(
     TaskEither.Do,
+    TaskEither.bind('newTag', () => TaskEither.of(core.getInput('ref'))),
+    TaskEither.bind('preTag', () =>
+      execute('/bin/bash -c "git tag --sort=-creatordate | sed -n 2p"')
+    ),
+    TaskEither.chain(({newTag, preTag}) =>
+      execute(
+        `git log --oneline --pretty=tformat:"%s by %an in %h" ${preTag.trim()}..${newTag}`
+      )
+    ),
+    TaskEither.bindTo('commitLog'),
     TaskEither.bind('commit_log', () =>
       TaskEither.of(core.getInput('commit_log'))
     ),
+    // TODO now can accept only "angular"
     TaskEither.bind('style', () => TaskEither.of(core.getInput('style'))),
     TaskEither.bind('scopes', () =>
       TaskEither.of(Option.of(core.getMultilineInput('scopes')))
     ),
-    TaskEither.chain(({commit_log, style, scopes}) =>
+    TaskEither.chain(({commitLog, style, scopes}) =>
       TaskEither.fromIO(
         categorize(
-          commit_log.split('\n'),
+          commitLog.split('\n'),
           Option.filter((s: string[]) => s.length != 0)(scopes)
         )
       )
